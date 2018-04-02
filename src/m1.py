2d0d51c3d374c69194f7ede9204d1fb8fd8d1f97
halenv <halenv@rose-hulman.edu>
2018-04-02 14:35:36 -0400

    m1 in progress

--- src/m1.py
+++ src/m1.py
@@ -108,6 +108,8 @@ class CircleChanger(object):
         self.circle.fill_color = fill_color
         self.colors = colors
 
+        self.original_color = fill_color
+
     def __repr__(self):
         """
         What comes in:
@@ -440,11 +442,13 @@ class CircleChanger(object):
                was constructed.
         """
         ################################################################
-        # TODO: 8.
+        # DONE: 8.
         #   First, READ the doc-string (specification) above.
         #   Second, READ the   run_test_change_to_original_color   function
         #   (below).  Third, implement and test this method.
         ################################################################
+
+        self.circle.fill_color = self.original_color
 
     def change_to_next_color_in_tuple(self):
         """
